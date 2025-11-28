39f1ea0c834540e867bcd935e3cfb217a4f90fad
JOÃO LUIS ANDRÉS RIQUELME DE ARAUJO <52274044880@imaccommand018.local>
2025-11-28 16:57:53 -0300

    pronto

---
+++ Objeto/objeto.js
@@ -0,0 +1,19 @@
+let ferrari = {
+modelo:"Laferrari" //característica - propriedades
+ano: "2025"
+frabricante:"Ferrari"
+cor:"Vermelho"
+
+acelerar() { //comportamentos - métodos
+    console.log("Acelerando...zum...zum...zum");
+},
+freiar(){
+    console.log("Freiando...");
+    
+}
+}
+
+console.log(ferrari.modelo);
+console.log(ferrari.acelerar());
+
+
